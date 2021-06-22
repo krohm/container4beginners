332224cc3538b1bdd87764c63bc5fb33061948e3
Michel <michael.kroh@basf.com>
2021-06-22 18:30:23 +0200

    skip_provider_registration

--- provider.tf
+++ provider.tf
@@ -17,6 +17,7 @@ provider "azurerm" {
   # Configuration options
   # See https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
   # https://github.com/terraform-providers/terraform-provider-azurerm
+  skip_provider_registration = true
   features {
     key_vault {
       purge_soft_delete_on_destroy = true
