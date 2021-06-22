526831bb80a0488b825c2b91a092f4a8c5195c66
Michel <michael.kroh@basf.com>
2021-06-22 19:36:11 +0200

    output

--- tf/acr.tf
+++ tf/acr.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "acr" {
-  name                     = "lx${var.index}"
+  name                     = "acr-${var.index}-${lower(substr(sha256(azurerm_app_service_plan.appserviceplan.id), 0, 6))}"
   resource_group_name      = "BASF_RG_LAB_${var.index}"
   location                 = "WestEurope"
   sku                      = "Standard"
