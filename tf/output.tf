0b6394d340448fadb6c98a317a88b3a0534470d6
Michel <michael.kroh@basf.com>
2021-06-22 19:36:22 +0200

    sensetiv

--- tf/output.tf
+++ tf/output.tf
@@ -1,2 +1,2 @@
 output "Readme0" {value ="Command to Login into Docker Registry"}
-output "Readme1" {value ="docker login ${azurerm_container_registry.acr.login_server} -u ${azurerm_container_registry.acr.admin_username} -p ${azurerm_container_registry.acr.admin_password}"}
+output "Readme1" {value ="docker login ${azurerm_container_registry.acr.login_server} -u ${azurerm_container_registry.acr.admin_username} -p ${azurerm_container_registry.acr.admin_password}" sensitive   = false}
