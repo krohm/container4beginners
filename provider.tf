0eb393f7c3da2072959427563cf4e8dbd75623a5
Michel <michael.kroh@basf.com>
2021-06-22 18:31:43 +0200

    skip_provider_registration = true

--- provider.tf
+++ provider.tf
@@ -5,6 +5,7 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 2.50"
+      skip_provider_registration = true
     }
     random = {
       source = "hashicorp/random"
@@ -17,7 +18,7 @@ provider "azurerm" {
   # Configuration options
   # See https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
   # https://github.com/terraform-providers/terraform-provider-azurerm
-  skip_provider_registration = true
+  
   features {
     key_vault {
       purge_soft_delete_on_destroy = true
