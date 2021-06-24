7cec9029cbba8e137440506f306c0f12f3bd4af1
Michel <michael.kroh@basf.com>
2021-06-24 14:54:10 +0200

    final version

--- tf/webapp.tf
+++ tf/webapp.tf
@@ -12,8 +12,6 @@ resource "azurerm_app_service_plan" "appserviceplan" {
     tier = "Standard"
     size = "S1"
   }
-
-  
     reserved = true # Mandatory for Linux plans
   
 }
@@ -21,23 +19,19 @@ resource "azurerm_app_service_plan" "appserviceplan" {
 # Create an Azure Web App for Containers in that App Service Plan
 resource "azurerm_app_service" "dockerapp" {
    name                = "dockerapp-${lower(substr(sha256(azurerm_app_service_plan.appserviceplan.id), 0, 6))}"
-
-  location            = "WestEurope"
-  resource_group_name = "BASF_RG_LAB_${var.index}"
-  app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
+   location            = "WestEurope"
+  resource_group_name  = "BASF_RG_LAB_${var.index}"
+  app_service_plan_id  = azurerm_app_service_plan.appserviceplan.id
 
   # Do not attach Storage by default
   app_settings= {
     WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
 
-    
     # Settings for private Container Registries  
     DOCKER_REGISTRY_SERVER_URL      = "https://${azurerm_container_registry.acr.name}.azurecr.io"
     DOCKER_REGISTRY_SERVER_USERNAME = azurerm_container_registry.acr.admin_username
     DOCKER_REGISTRY_SERVER_PASSWORD = azurerm_container_registry.acr.admin_password
-    WEBSITES_PORT=8080
-  
-    
+    WEBSITES_PORT=8080  
   }
 
   # Configure Docker Image to load on start
