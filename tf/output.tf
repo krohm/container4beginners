8b7c2b02fc682f32bede4a1d339d95cb4772e59c
Michel <michael.kroh@basf.com>
2021-06-24 14:54:20 +0200

    final version

--- tf/output.tf
+++ tf/output.tf
@@ -1,2 +1,17 @@
-output "Readme0" {value ="Command to Login into Docker Registry"}
-output "Readme1" {value ="docker login ${azurerm_container_registry.acr.login_server} -u ${azurerm_container_registry.acr.admin_username} -p ${azurerm_container_registry.acr.admin_password}" sensitive   = false}
+output "Readme0" {
+    value ="Parameters to Login into Docker Registry"
+}
+
+
+output "acr-name" {
+    sensitive = false
+    value =azurerm_container_registry.acr.login_server
+    }
+output "acr-username" {
+    sensitive = false
+    value =azurerm_container_registry.acr.admin_username
+    }
+output "acr-password" {
+    sensitive = false
+    value ="Have to be obtained from the Azure Portal"
+    }
